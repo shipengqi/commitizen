922d4fbeb909fd2ba5d41b0adac0c2db166ed369
shipengqi <pooky.shipengqi@gmail.com>
2024-03-12 16:58:19 +0800

    feat(test): add more unit tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,11 +1,41 @@
-package config
+package config_test
 
-import "testing"
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/shipengqi/commitizen/internal/config"
+)
 
 func TestLoadTemplates(t *testing.T) {
-	ts, err := LoadTemplates("./testdata/.git-czrc")
-	if err != nil {
-		t.Log(err)
+	tests := []struct {
+		title  string
+		tmpl   string
+		expect int
+	}{
+		{
+			"without any default templates",
+			"./testdata/.git-czrc",
+			2,
+		},
+		{
+			"with one default templates",
+			"./testdata/.git-czrc-with-default",
+			2,
+		},
+		{
+			"with two default templates",
+			"./testdata/.git-czrc-with-two-default",
+			2,
+		},
 	}
-	t.Log(ts)
+	g := NewWithT(t)
+	for _, v := range tests {
+		t.Run(v.title, func(t *testing.T) {
+			ts, err := config.LoadTemplates(v.tmpl)
+			g.Expect(err).To(BeNil())
+			g.Expect(len(ts)).To(Equal(v.expect))
+		})
+	}
 }
