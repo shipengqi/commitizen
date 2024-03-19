4f29a495100347ac6ad80888e44f0b0863656a43
shipengqi <pooky.shipengqi@gmail.com>
2024-03-19 10:20:39 +0800

    feat(options): add a new option --default/-t

--- internal/options/options.go
+++ internal/options/options.go
@@ -26,7 +26,7 @@ func (o *Options) AddFlags(f *pflag.FlagSet) {
 
 	f.BoolVar(&o.DryRun, "dry-run", o.DryRun, "you can use the --dry-run flag to preview the message that would be committed, without really submitting it.")
 	f.StringVarP(&o.Template, "template", "t", o.Template, "template name to use when multiple templates exist.")
-	f.BoolVarP(&o.Default, "default", "d", o.Default, "use the default template, same as '--template default'. '--default' has a higher priority than '--template'.")
+	f.BoolVarP(&o.Default, "default", "d", o.Default, "use the default template, '--default' has a higher priority than '--template'.")
 	f.BoolVar(&o.NoTTY, "no-tty", o.NoTTY, "make sure that the TTY (terminal) is never used for any output.")
 
 	_ = f.MarkHidden("no-tty")
