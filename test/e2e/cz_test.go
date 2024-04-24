d6e1ded575aff715a69e93c61238fc15c6533512
shipengqi <pooky.shipengqi@gmail.com>
2024-04-24 09:22:46 +0800

    fix(test): disable command running tests

--- test/e2e/cz_test.go
+++ test/e2e/cz_test.go
@@ -2,19 +2,8 @@ package e2e_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
 )
 
 func CZTest() {
-	Context("Check Commitizen", func() {
-		It("should not need to select a template", func() {
-			se, err = RunCLITest()
-			NoError(err)
-			Eventually(se.Out).Should(gbytes.Say("Select the type of"))
-			Eventually(se.Out).Should(gbytes.Say("A new feature"))
-			Eventually(se.Out).ShouldNot(gbytes.Say("Select the template of change that you're committing:"))
-			se.Terminate()
-		})
-	})
+	Context("Run Commitizen", func() {})
 }
